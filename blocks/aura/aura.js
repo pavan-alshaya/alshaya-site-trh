a165bac511c0e338c931fc38d60cf63e10781fcf
Sabarinath T <sabarinath.t@alshaya.com>
2024-06-11 18:26:31 +0530

    LDHUB-51590: aura-page with lint fix.

--- blocks/aura/aura.js
+++ blocks/aura/aura.js
@@ -2,7 +2,6 @@ export default async function decorate() {
   const topSection = document.querySelector('.benifits-details');
   const topSectionChildEls = topSection.querySelectorAll('p');
   topSectionChildEls.forEach((a, i) => {
-    a.classList.add('col-level-' + i);
+    a.classList.add(`col-level-${i}`);
   });
 }
-
