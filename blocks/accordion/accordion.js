be45f1d453843d0d207efa88f8455b57f28b9c83
Veera Raghava Pavan <v.naram@DXBLT03066.local>
2024-06-14 14:26:56 +0400

    menu page and UI improvements

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -29,5 +29,48 @@ export default function decorate(block) {
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
+
+    // Wrappering the Accordion inside another accordion
+    const accordionItems = document.querySelectorAll('.accordion-wrapper');
+    const accordionBodyItem = document.querySelector('.accordion-item .accordion-item-body');
+    accordionItems.forEach((accordion) => {
+      const accordionItemChildren = accordion.querySelectorAll('.accordion .accordion-item');
+      if (accordionItemChildren.length > 1) {
+        let i = 1;
+        while (i < accordionItemChildren.length) {
+          accordionBodyItem.append(accordionItemChildren[i]);
+          i += 1;
+        }
+      }
+    });
+
+    // Wrapp the accordion wrapper elemenets to new accordion wrapper container element
+    // const accordionContainerEl = document.querySelector('.accordion-container');
+    // const accordionContainerWrapperEl = document.createElement('div');
+    // accordionContainerWrapperEl.className = 'accordion-wrapper-container';
+    // accordionContainerEl.appendChild(accordionContainerWrapperEl);
+    // const childElements = accordionContainerEl.querySelectorAll('.accordion-wrapper');
+    // childElements.forEach(child => {
+    //   accordionContainerWrapperEl.appendChild(child);
+    // });
+
+    // Wrap the child accordion details elements in to the div
+    const detailsElements = document.querySelectorAll('.accordion-item .accordion-item');
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('child-accordion-wrapper');
+    detailsElements.forEach((detailsElement) => {
+      detailsElement.parentNode.removeChild(detailsElement);
+      wrapper.appendChild(detailsElement);
+    });
+    // remmove empty child-accordion-wrapper elements
+    const els = document.querySelectorAll('.child-accordion-wrapper');
+    els.forEach((el) => {
+      const childElements = el.children;
+      if (childElements.length === 0) {
+        el.parentNode.removeChild(el);
+      }
+    });
+    const parentElement = document.querySelector('.accordion-item .accordion-item-body');
+    parentElement.appendChild(wrapper);
   });
 }
---
+++ blocks/menuPage/menuPage.js
@@ -0,0 +1,119 @@
+// Make the .menu-nav-section to be fixed on scroll
+const defaultContentWrapper = document.querySelector('.menu-nav-section .default-content-wrapper');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 400) {
+    defaultContentWrapper.style.position = 'fixed';
+    defaultContentWrapper.style.top = '0';
+    defaultContentWrapper.style.left = '0';
+    defaultContentWrapper.style.width = '100%';
+    defaultContentWrapper.style.zIndex = '2';
+  } else {
+    defaultContentWrapper.style.position = 'static';
+  }
+
+  // Reset styles for mobile resolution
+  if (window.innerWidth <= 768) {
+    defaultContentWrapper.style.position = 'static';
+  }
+});
+
+// Smooth scroll effect
+const anchorLinks = document.querySelectorAll('.menu-nav-section .default-content-wrapper a');
+anchorLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Change Country logic
+const targetNode = document.body;
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationsList, observer) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      const topNavEl = document.querySelector('.top-nav-section .top-nav-el-0');
+      const changeCountryEl = document.querySelectorAll('.top-navlist-item1 li');
+
+      changeCountryEl.forEach((liElement) => {
+        liElement.addEventListener('click', () => {
+          const clickedText = liElement.textContent.trim();
+          topNavEl.textContent = clickedText;
+
+          // Store the selected item in local storage
+          localStorage.setItem('selectedCountry', clickedText);
+
+          // Reload the page
+          window.location.reload();
+        });
+      });
+
+      const selectedCountry = localStorage.getItem('selectedCountry');
+
+      if (selectedCountry) {
+        topNavEl.textContent = selectedCountry;
+        const showElement = (selector) => {
+          const elements = document.querySelectorAll(selector);
+          elements.forEach((element) => {
+            element.style.display = 'block';
+          });
+        };
+        const hideElement = (selector) => {
+          const elements = document.querySelectorAll(selector);
+          elements.forEach((element) => {
+            element.style.display = 'none';
+          });
+        };
+        switch (selectedCountry) {
+          case 'Kuwait':
+            showElement('.kuwait');
+            hideElement('.behrain');
+            break;
+          case 'Bahrain':
+            showElement('.behrain');
+            hideElement('.kuwait');
+            break;
+          default:
+            showElement('.kuwait');
+            showElement('.behrain');
+            break;
+        }
+
+        observer.disconnect();
+      }
+
+      const accordionContainerEl = document.querySelector('.lunch-value-offers.kuwait');
+      const accordionContainerWrapperEl = document.createElement('div');
+      accordionContainerWrapperEl.className = 'accordion-wrapper-container';
+      accordionContainerEl.appendChild(accordionContainerWrapperEl);
+      const childElements = accordionContainerEl.querySelectorAll('.accordion-wrapper');
+      childElements.forEach((child) => {
+        accordionContainerWrapperEl.appendChild(child);
+      });
+
+      const accordionContainerEl1 = document.querySelector('.lunch-value-offers.behrain');
+      const accordionContainerWrapperEl1 = document.createElement('div');
+      accordionContainerWrapperEl1.className = 'accordion-wrapper-container';
+      accordionContainerEl1.appendChild(accordionContainerWrapperEl1);
+      const childElements1 = accordionContainerEl1.querySelectorAll('.accordion-wrapper');
+      childElements1.forEach((child) => {
+        accordionContainerWrapperEl1.appendChild(child);
+      });
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
